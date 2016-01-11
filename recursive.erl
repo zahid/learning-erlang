baa3d53ab649816f62f49ebfccbe559a55dcb69d
Zahid Mahir <zahid@zmhr.me>
2016-01-10 23:15:26 -0500

    Added more tail recursive functions

--- recursive.erl
+++ recursive.erl
@@ -7,7 +7,13 @@
     duplicate/2,
     tail_duplicate/2,
     reverse/1,
-    tail_reverse/1]).
+    tail_reverse/1,
+    sublist/2,
+    tail_sublist/2,
+    zip/2,
+    lenient_zip/2,
+    tail_zip/2,
+    tail_lenient_zip/2]).
 
 len([]) -> 0;
 len([_|T]) -> 1 + len(T).
@@ -42,3 +48,39 @@ tail_reverse(L) -> tail_reverse(L,[]).
 tail_reverse([],Acc) -> Acc;
 tail_reverse([H|T], Acc) -> tail_reverse(T, [H|Acc]).
 
+sublist(_,0) -> [];
+sublist([],_) -> [];
+sublist([H|T],N) when N > 0 -> [H|sublist(T,N-1)].
+
+tail_sublist(List,N) ->
+    lists:reverse(tail_sublist(List,N,[])).
+
+tail_sublist(_,0,Sublist) -> Sublist;
+tail_sublist([],_,Sublist) -> Sublist;
+tail_sublist([H|T],N,Sublist) ->
+    tail_sublist(T,N-1,[H|Sublist]).
+
+zip([],[]) -> [];
+zip([X|Xs],[Y|Ys]) ->
+    [{X,Y}|zip(Xs,Ys)].
+
+lenient_zip([],[]) -> [];
+lenient_zip([],[Y|Ys]) ->
+    [{empty,Y}|lenient_zip([],Ys)];
+lenient_zip([X|Xs],[]) ->
+    [{X,empty}|lenient_zip(Xs,[])];
+lenient_zip([X|Xs],[Y|Ys]) ->
+    [{X,Y}|lenient_zip(Xs,Ys)].
+
+tail_zip(X,Y) -> lists:reverse(tail_zip(X,Y,[])).
+
+tail_zip([],_,Acc) -> Acc;
+tail_zip(_,[],Acc) -> Acc;
+tail_zip([X|Xs],[Y|Ys],Acc) -> tail_zip(Xs,Ys,[{X,Y}|Acc]).
+
+tail_lenient_zip(X,Y) -> lists:reverse(tail_lenient_zip(X,Y,[])).
+
+tail_lenient_zip([],[],Acc) -> Acc;
+tail_lenient_zip([X|Xs],[],Acc) -> tail_lenient_zip(Xs,[],[{X,empty}|Acc]);
+tail_lenient_zip([],[Y|Ys],Acc) -> tail_lenient_zip([],Ys,[{empty,Y}|Acc]);
+tail_lenient_zip([X|Xs],[Y|Ys],Acc) -> tail_lenient_zip(Xs,Ys,[{X,Y}|Acc]).
